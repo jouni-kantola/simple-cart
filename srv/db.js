5f6b78dfe7f183e4601e513a61dc05dc1d905d41
Jouni Kantola <jouni@kantola.se>
2014-10-14 13:43:33 +0200

    CRUD

--- srv/db.js
+++ srv/db.js
@@ -2,11 +2,17 @@ var fs = require('fs'),
     collections = {}
 
 function ensureCollection(name) {
+    var initData, parsedData
+
     if (!collections[name]) {
-        var initData = fs.readFileSync(__dirname + '/db/' + name + '.json', {
+        initData = fs.readFileSync(__dirname + '/db/' + name + '.json', {
             encoding: 'utf8'
         })
-        collections[name] = JSON.parse(initData)
+        parsedData = JSON.parse(initData)
+        collections[name] = parsedData.reduce(function(dictionary, curr, index) {
+            dictionary[index] = curr
+            return dictionary
+        }, {})
     }
     return collections[name]
 }
@@ -21,9 +27,23 @@ module.exports = {
             filter: {
                 by: function(property, value) {
                     return coll.filter(function(item) {
-                        return item[property] === value;
-                    });
+                        return item[property] === value
+                    })
+                },
+                byId: function(id) {
+                    return coll[id]
                 }
+            },
+            add: function(object) {
+                var index = Object.keys(coll).length;
+                coll[index] = object
+                return index
+            },
+            update: function(id, object) {
+                coll[id] = object
+            },
+            delete: function(id) {
+                delete coll[id];
             }
         }
     }
