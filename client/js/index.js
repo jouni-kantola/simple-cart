242eb6b1253c9a046261bc204f813b634da42ef3
Jouni Kantola <jouni@kantola.se>
2014-10-15 10:36:27 +0200

    Get product details

--- client/js/index.js
+++ client/js/index.js
@@ -2,7 +2,25 @@ var rivets = require('rivets');
 var products = require('./products');
 
 (function() {
-    rivets.bind(document.getElementById('products'), {
+    var productList = document.getElementById('products'),
+        productDetails = document.getElementById('product-details')
+
+    rivets.bind(productList, {
         products: products.all
     })
+    rivets.bind(productDetails, {
+        product: products.current
+    })
+
+    productList.addEventListener('click', function(e) {
+        if (e.target.nodeName.toLowerCase() === 'span') {
+            var productId = +(e.target.getAttribute('data-id'))
+            products.one(productId, function() {
+                console.log(productDetails)
+                console.log(products)
+                productDetails.classList.remove('hide')
+            })
+            return false;
+        }
+    });
 })()
--- client/js/products.js
+++ client/js/products.js
@@ -1,20 +1,34 @@
 var apiClient = require('./api-client');
 var products = [];
+var current = {};
 
 (function() {
+    list();
+})()
+
+function list() {
     apiClient.get('/products').then(function(data) {
         Object.keys(data).forEach(function(key) {
             var o = data[key];
             products.push({
-            	href: '/products/' + key,
+                href: '/products/' + key,
                 id: key,
                 uid: o.id,
                 name: o.name
             });
         })
     })
-})()
+}
+
+function one(id, callback) {
+    apiClient.get('/products/' + id).then(function(data) {
+        current = data
+        callback()
+    })
+}
 
 module.exports = {
-    all: products
+    all: products,
+    one: one,
+    current: current
 }
